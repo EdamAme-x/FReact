e000f778b34de65a492a24169ee515652d790b71
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-09-01 15:48:06 +0900

    fix

--- example/routes/_app.tsx
+++ example/routes/_app.tsx
@@ -1,9 +1,8 @@
 import { AppProps } from "$fresh/server.ts";
+import { $ } from "freact";
 
 export default function App({ Component }: AppProps) {
-  return (
-    <>
-      <Component />
-    </>
+  return $.Fragment(
+    Component()
   );
 }
